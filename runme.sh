a774d3727c7ddbbcfb1a1050d6f31c110cbae6d8
Tim <sakunamary@gmail.com>
2018-10-28 13:20:10 +0000

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -15,17 +15,28 @@ echo "install pixel"
 
 sudo apt-get -y install xorg
 sudo apt-get -y install lxde openbox
-sudo apt-get -y install pix-icons pix-plym-splash rpd-wallpaper
+sudo apt-get -y install pix-icons pix-plym-splash 
+#sudo apt-get -y install rpd-wallpaper
 sudo apt-get -y install raspberrypi-ui-mods
 sudo apt-get -y install ttf-wqy-zenhei 
+sudo apt-get -y install git
 echo "--------finish install pixel ------------ "
 
 echo "install arduino "
 sudo apt-get -y install arduino 
+sudo apt-get -y install chromium-browser
+
+
+cd /home/pi/Downloads
+git clone https://github.com/sakunamary/AVRDuino.git
+cd /home/pi/Downloads/AVRDuino
+sudo chmod +x ./avrduino.sh install
+
+
 echo "finish arduino installing..."
 
 echo "setup python dev "
 
-sudo apt-get -y install python-dev python3-dev  python-pip python3-pip
+sudo apt-get -y install python-dev python3-dev  python-pip python3-pip python-serial
 
 echo "finished setup python dev "
